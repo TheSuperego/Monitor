57159cea29bd8aea35517b1fef381559c61acae9
Tosuke <1848228664@qq.com>
2022-08-20 23:51:20 +0800

    封装websocket函数，与后端联调成功。

--- packages/manage-system/src/api/tool/socket.ts
+++ packages/manage-system/src/api/tool/socket.ts
@@ -1,7 +1,9 @@
 function sockets(){
-    const socket = new WebSocket ('localhost:9000')
+    const socket = new WebSocket ('ws://localhost:9000')
     socket.addEventListener('open', () => {
        console.log('连接成功')
+       socket.send("连接成功啦！")
     })
+
 }
 export default sockets
